81124653a100fe7e29a43924128cb6effa8d83ae
JYZR GRFX <jayzurgandia@gmail.com>
2020-10-14 15:53:27 +0800

    Added covidTracker project

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,25 +15,6 @@ const Project = () => {
                     <a href="https://covidtracker-angular.web.app/" target="_blank" rel="noopener noreferrer">View Project</a>
                 </li>
 
-                <li className="project-item">
-                    <h2>PROJECT 2</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 
-                        1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book.It has survived not only five centuries, 
-                        but also the leap into electronic typesetting, remaining essentially unchanged. 
-                    </p>
-                </li>
-
-                <li className="project-item">
-                    <h2>PROJECT 3</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 
-                        1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book. It has survived not only five centuries, 
-                        but also the leap into electronic typesetting, remaining essentially unchanged. 
-                    </p>
-                </li>
             </ul>
 
             <h2 className="heading">ME AS AN ILLUSTRATOR</h2>
